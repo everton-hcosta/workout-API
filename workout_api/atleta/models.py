2a64c8e9761bd2ba18ba44683cde43e411f93fc8
everton-hcosta <everton.hcosta@gmail.com>
2025-07-26 11:13:08 -0300

    :construction:Criação das rotas de Categoria

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -17,10 +17,10 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atletas")
+    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
     categoria_id: Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
     centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(
-        back_populates="atletas"
+        back_populates="atleta"
     )
     centro_treinamento_id: Mapped[int] = mapped_column(
         ForeignKey("centros_treinamento.pk_id")
--- workout_api/routers.py
+++ workout_api/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from workout_api.atleta.controller import router as atleta
+from workout_api.categorias.controller import router as categoria
 
 api_router = APIRouter()
 
 api_router.include_router(atleta, prefix="/atleta", tags=["atleta"])
+api_router.include_router(categoria, prefix="/categoria", tags=["categoria"])
